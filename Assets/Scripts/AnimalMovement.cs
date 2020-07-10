0f68dcc45038db0a50749cbda52e8315cdb7f381
Raijin2020 <68131337+Raijin2020@users.noreply.github.com>
2020-07-10 18:49:56 -0400

    Also did something
    success

--- Assets/Scripts/AnimalMovement.cs
+++ Assets/Scripts/AnimalMovement.cs
@@ -14,7 +14,8 @@ public class AnimalMovement : MonoBehaviour {
 	void Start() {
 		//choose a random start direction
 		startDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
-		//Hi Nick
+		//Hi Nick, Hi Connor 
+			
 		currentDirection = startDirection;
 	}
 
