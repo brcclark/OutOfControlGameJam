623ed84008866f8e0d0d144358231cfe5aef5303
Raijin2020 <68131337+Raijin2020@users.noreply.github.com>
2020-07-10 22:06:38 -0400

    stopping in the pen
    figuring out how to make them stop in the pen

--- Assets/Scripts/AnimalMovement.cs
+++ Assets/Scripts/AnimalMovement.cs
@@ -15,6 +15,7 @@ public class AnimalMovement : MonoBehaviour {
 	Vector2 playerToSheepDir;
 	Vector2 currentDirection;
 	Transform player;
+	Transform pen;
 
 	bool inPen = false;
 
@@ -22,6 +23,7 @@ public class AnimalMovement : MonoBehaviour {
 	void Start() {
 		//Bringing in the player position
 		player = GameObject.FindGameObjectWithTag("Player").transform;
+		pen = GameObject.FindGameObjectWithTag("Pen").transform;
 
 		//choose a random start direction
 		startDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
@@ -31,27 +33,32 @@ public class AnimalMovement : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D col) {
 		if (col.gameObject.CompareTag("Pen")) {
 			inPen = true;
+			print("Hey I'm in the pen");
 		}
 	}
 	void OnTriggerExit2D(Collider2D col) {
 		if (col.gameObject.CompareTag("Pen")) {
 			inPen = false;
+			print("Yo I'm out!");
 		}
 	}
 	// Update is called once per frame
 	void Update() {
 		//Determine the next position
+		if(!inPen){
 		//Check to see if the player is in our Zone of influence
-		PlayerVisible();
+			PlayerVisible();
 		//Check to see if we are in the pen
+			InPen();
 		//If none of that has happened, we'll just check to randomly change our direction
-		RandomPositionTimer();
+			RandomPositionTimer();
+		}
 		//Update the position
 		UpdatePosition();
 	}
 
 	void RandomPositionTimer() {
-		if (currentMoveTimer < timeUntilDirChange) {
+		if ((currentMoveTimer < timeUntilDirChange) && !inPen){
 			currentMoveTimer += Time.deltaTime;
 		}
 		currentMoveTimer = Mathf.Clamp(currentMoveTimer, 0, timeUntilDirChange);
@@ -69,9 +76,16 @@ public class AnimalMovement : MonoBehaviour {
 	void PlayerVisible() {
 		if(Vector2.Distance(transform.position,player.position) <= scareDistance){
 			playerToSheepDir = (transform.position- player.position).normalized;
-			print(Vector2.Distance(transform.position,player.position));
 			currentDirection = playerToSheepDir;
 			currentMoveTimer = 0f;
 		}
+	}
+
+	void InPen() {
+		if(inPen){
+			currentDirection = Vector2.zero;
+			currentMoveTimer = 0f;
+		}
+
 	}
 }
