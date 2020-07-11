b0a674fbe9bf9fec94f490d967a7c08781997e41
Raijin2020 <68131337+Raijin2020@users.noreply.github.com>
2020-07-10 20:16:31 -0400

    did the collab join
    because

--- Assets/Scripts/AnimalMovement.cs
+++ Assets/Scripts/AnimalMovement.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class AnimalMovement : MonoBehaviour {
 	public float moveRate;
 	public float moveSpeed = 5f;
+	//for Sheep timer to change direction
+	public float timeUntilDirChange = 3f;
+	public float scareDistance = 10f;
 	public Vector2 startDirection;
 
 	float lastMoveTime;
+	float currentMoveTimer;
 	Vector2 currentDirection;
+	Transform player;
 
 	// Start is called before the first frame update
 	void Start() {
+		//Bringing in the player position
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+
 		//choose a random start direction
 		startDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
-		//Hi Nick, Hi Connor 
+		//Hi Nick, Unity?
 			
 		currentDirection = startDirection;
 	}
@@ -22,5 +30,15 @@ public class AnimalMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update() {
 		transform.position += (Vector3)currentDirection * moveSpeed * Time.deltaTime;
+		
+		if(currentMoveTimer < timeUntilDirChange) {
+			currentMoveTimer += Time.deltaTime;
+		}
+		currentMoveTimer = Mathf.Clamp(currentMoveTimer, 0, timeUntilDirChange);
+		
+		if(currentMoveTimer >= timeUntilDirChange){
+			currentDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+			currentMoveTimer = 0f;
+		}
 	}
 }
