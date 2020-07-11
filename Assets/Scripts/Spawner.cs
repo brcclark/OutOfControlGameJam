7e89e4ea70a23afe7b4c4cb176671961c5f52f76
dipsmac <dipsmac@gmail.com>
2020-07-10 23:37:13 -0400

    Spawner on interval
    Sheep spawn on interval now instead of in a wave

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,21 +5,36 @@ using UnityEngine;
 public class Spawner : MonoBehaviour {
 
 	public Transform sheep;
-	public int numSheep = 3;
+	public int startingSheep = 3;
+	public float spawnInterval = 10;
 
 	Transform t;
-
+	float spawnTimer;
 	// Start is called before the first frame update
 	void Start() {
 		t = GetComponent<Transform>();
-		for (int i = 0; i < numSheep; i++) {
-			Transform sp = Instantiate(sheep, new Vector3(Random.Range(-15f, 15f), Random.Range(-15f, 15f), 0), Quaternion.identity) as Transform;
-			sp.parent = t;
+		for (int i = 0; i < startingSheep; i++) {
+			SpawnSheep();
+		}
+	}
+
+	void SpawnSheep() {
+		Transform sp = Instantiate(sheep, new Vector3(Random.Range(-15f, 15f), Random.Range(-15f, 15f), 0), Quaternion.identity) as Transform;
+		sp.parent = t;
+	}
+
+	void CheckSheepSpawner() {
+		if ((spawnTimer < spawnInterval)) {
+			spawnTimer += Time.deltaTime;
+		}
+		else {
+			spawnTimer = 0;
+			SpawnSheep();
 		}
 	}
 
 	// Update is called once per frame
 	void Update() {
-
+		CheckSheepSpawner();
 	}
 }
