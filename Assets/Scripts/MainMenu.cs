883091eaaf21639095b8091f6195044f896f938d
Raijin2020 <68131337+Raijin2020@users.noreply.github.com>
2020-07-11 23:04:35 -0400

    Menu's swap screens and scenes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,10 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
     public void PlayGame(){
         SceneManager.LoadScene("GameScene");
     }
     public void ExitGame(){
         Application.Quit();
     }
+    public void Options(){
+         mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+    public void Back(){
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
 }
+
