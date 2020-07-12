0858defcd5ea111c5efaf69dd9b7f800dd7c5336
dipsmac <dipsmac@gmail.com>
2020-07-11 22:09:32 -0400

    Added a bark to the dog, added some doodads and additional graphics

---
+++ Assets/Scripts/Bark.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bark : MonoBehaviour {
+	float speed = 5;
+	float lifetime = 3;
+	// Start is called before the first frame update
+	void Start() {
+		Destroy(gameObject, lifetime);
+	}
+
+	void Update() {
+		float distanceToMove = speed * Time.deltaTime;
+		transform.Translate(Vector3.right * distanceToMove);
+	}
+}
