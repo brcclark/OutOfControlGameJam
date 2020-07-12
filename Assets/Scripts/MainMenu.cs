4102478818f4ad7ae69b9ad9a2bfd6b9fd48065b
Raijin2020 <68131337+Raijin2020@users.noreply.github.com>
2020-07-11 23:23:47 -0400

    Fixed 2 audio listeners. Made volume slider work

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     public GameObject mainMenu;
     public GameObject optionsMenu;
+    public Slider musicSlider;
     public void PlayGame(){
         SceneManager.LoadScene("GameScene");
     }
@@ -14,12 +16,18 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
     }
     public void Options(){
-         mainMenu.SetActive(false);
+        mainMenu.SetActive(false);
         optionsMenu.SetActive(true);
     }
     public void Back(){
         mainMenu.SetActive(true);
         optionsMenu.SetActive(false);
+    }
+    public void UpdateMusicVolume(){
+        PlayerPrefs.SetFloat("MusicVolume", musicSlider.value);
+    }
+    void Start(){
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume", 0.75f);
     }
 }
 
